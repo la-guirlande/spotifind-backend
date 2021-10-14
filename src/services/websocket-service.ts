ba1a16b7aaa2444f2e44fd4b37ef410f7ff2bb92
Lelberto <jsurieux@gmail.com>
2021-10-14 18:38:05 +0200

    Add ERROR, TEST, JOIN and CONNECT events

--- src/services/websocket-service.ts
+++ src/services/websocket-service.ts
@@ -1,11 +1,23 @@
+import _ from 'lodash';
+import { Error as MongooseError } from 'mongoose';
 import { Server, Socket } from 'socket.io';
+import { GameInstance, PlayerAttributes, Status } from '../models/game-model';
+import { APIErrorResponse } from './error-service';
 import Service from './service';
 import ServiceContainer from './service-container';
+import { GameTokenData } from './token-service';
 
 /**
  * Websocket service class.
  * 
  * This service is used to manage the websocket server.
+ * 
+ * Webocket workflow :
+ *  - Game registration :
+ *    - The author creates a new game, gets game code and player token (API)
+ *    - A player joins game with the code and gets player token (`JOIN` event)
+ *  - Game connection :
+ *    - With game code and player token, players (and the author) connects their websocket to the game room (`CONNECT` event)
  */
 export default class WebsocketService extends Service {
 
@@ -62,7 +74,140 @@ export default class WebsocketService extends Service {
           this.logger.info(`Websocket disconnected : ${socket.handshake.address}`);
       });
 
-      // Websocket logic here
+      // Used for testing websocket connection between server and client (dev only)
+      if (this.container.env.nodeEnv === 'development') {
+        socket.on(EventType.TEST, (data: TestClientToServerEvent) => {
+          this.logger.debug('Websocket test received with data :', data);
+          return socket.emit(EventType.TEST, { status: 'OK', data } as TestServerToClientEvent);
+        });
+      }
+
+      // When the socket wants to join a game
+      socket.on(EventType.JOIN, async (data: JoinClientToServerEvent) => {
+        try {
+          const game = await this.db.games.findOne().where('code').equals(data.code);
+          if (game == null) {
+            return socket.emit(EventType.ERROR, this.container.errors.formatErrors({ error: 'not_found', error_description: 'Invalid code' }) as ErrorEvent);
+          }
+          if (game.status !== Status.INIT) {
+            let error_description;
+            switch (game.status) {
+              case Status.IN_PROGRESS:
+                error_description = 'Game in progress';
+                break;
+              case Status.FINISHED:
+                error_description = 'Game finished';
+                break;
+              default:
+                error_description: 'Game not accessible';
+                break;
+            }
+            return socket.emit(EventType.ERROR, this.container.errors.formatErrors({ error: 'access_denied', error_description }) as ErrorEvent);
+          }
+          game.players.push({ name: data.name });
+          await game.save();
+          return socket.emit(EventType.JOIN, { token: await game.generateToken(_.last(game.players).id) } as JoinServerToClientEvent);
+        } catch (err) {
+          this.logger.error(err);
+          if (err instanceof MongooseError.ValidationError) {
+            return socket.emit(EventType.ERROR, this.container.errors.formatErrors(...this.container.errors.translateMongooseValidationError(err)) as ErrorEvent);
+          }
+          return socket.emit(EventType.ERROR, this.container.errors.formatServerError() as ErrorEvent);
+        }
+      });
+
+      // When the socket wants to connect to a game
+      socket.on(EventType.CONNECT, async (data: ConnectClientToServerEvent) => {
+        try {
+          const tokenData = await this.container.tokens.decode<GameTokenData>(data.token, process.env.GAME_TOKEN_KEY);
+          const game = await this.db.games.findOne().where('code').equals(tokenData.code);
+          if (game == null) {
+            return socket.emit(EventType.ERROR, this.container.errors.formatErrors({ error: 'not_found', error_description: 'Invalid code' }) as ErrorEvent);
+          }
+          if (game.status === Status.FINISHED) {
+            return socket.emit(EventType.ERROR, this.container.errors.formatErrors({ error: 'access_denied', error_description: 'Game is finished' }) as ErrorEvent);
+          }
+          const player = game.players.find(player => player.id === tokenData.playerId);
+          if (player == null) {
+            return socket.emit(EventType.ERROR, this.container.errors.formatErrors({ error: 'not_found', error_description: 'Invalid token' }) as ErrorEvent);
+          }
+          socket.join(game.id);
+          socket.emit(EventType.CONNECT, { game, player } as ConnectServerToClientEvent);
+          return socket.to(game.id).emit(EventType.CONNECT, { player } as ConnectServerToBroadcastEvent);
+        } catch (err) {
+          this.logger.error(err);
+          if (err instanceof MongooseError.ValidationError) {
+            return socket.emit(EventType.ERROR, this.container.errors.formatErrors(...this.container.errors.translateMongooseValidationError(err)) as ErrorEvent);
+          }
+          return socket.emit(EventType.ERROR, this.container.errors.formatServerError() as ErrorEvent);
+        }
+      });
     });
   }
+}
+
+enum EventType {
+  ERROR = 'error', TEST = 'test', JOIN = 'join', CONNECT = 'co'
+}
+
+/**
+ * Base websocket event.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Event {}
+
+/**
+ * Error event.
+ */
+interface ErrorEvent extends Event, APIErrorResponse {}
+
+/**
+ * Test event (client to server).
+ */
+interface TestClientToServerEvent extends Event {
+  [key: string]: unknown;
+}
+
+/**
+ * Test event (server to client).
+ */
+interface TestServerToClientEvent extends Event {
+  [key: string]: unknown;
+}
+
+/**
+ * Join event (client to server).
+ */
+interface JoinClientToServerEvent extends Event {
+  code: string;
+  name: string;
+}
+
+/**
+ * Join event (server to client).
+ */
+interface JoinServerToClientEvent extends Event {
+  token: string;
+}
+
+/**
+ * Connect event (client to server).
+ */
+interface ConnectClientToServerEvent extends Event {
+  token: string;
+}
+
+/**
+ * Connect event (server to client).
+ */
+interface ConnectServerToClientEvent extends Event {
+  game: GameInstance;
+  player: PlayerAttributes;
+}
+
+/**
+ * Connect event (server to broadcast).
+ */
+interface ConnectServerToBroadcastEvent extends Event {
+  player: PlayerAttributes;
 }
